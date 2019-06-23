5df91dadfb8d3f646c2cbbb7798147a674f66223
Vortex Acherontic <vortex@z-ray.de>
2019-06-23 17:01:39 +0200

    Stuff I dont know

--- config/Config.py
+++ config/Config.py
@@ -2,8 +2,21 @@ import json
 
 from utils import Const, JSONLoader
 
+MODULE_TWITCH = 0
+MODULE_TWITTER = 1
+MODULE_YOUTUBE = 2
+MODULE_PATREON = 3
+MODULE_STEAM = 4
+MODULE_TRELLO = 5
+
 
 class Config:
-    def __init__(self, botJSON, tiwtchJSON):
+    def __init__(self, botJSON, twitchJSON):
         self.botname = botJSON["botname"]
-        print("Bot Name: " + self.botname)
+        self.modules = [None] * 6
+        self.modules[MODULE_TWITCH] = botJSON["modules"]["twitch"]
+        self.modules[MODULE_TWITTER] = botJSON["modules"]["twitter"]
+        self.modules[MODULE_YOUTUBE] = botJSON["modules"]["youtube"]
+        self.modules[MODULE_PATREON] = botJSON["modules"]["patreon"]
+        self.modules[MODULE_STEAM] = botJSON["modules"]["steam"]
+        self.modules[MODULE_TRELLO] = botJSON["modules"]["trello"]
