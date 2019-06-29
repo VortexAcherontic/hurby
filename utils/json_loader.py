43b09a53d348e9dabee6703ab942c16eb10d7f31
Vortex Acherontic <vortex@z-ray.de>
2019-06-30 01:06:36 +0200

    #1 added save json uction to jsonLoader

--- utils/json_loader.py
+++ utils/json_loader.py
@@ -3,9 +3,14 @@ import json
 from utils import logger
 
 
-def loadJSON(file: str):
+def loadJSON(file: str) -> dict:
     with open(file) as f:
         logger.log(logger.INFO, "Loading JSON: " + file)
         d = json.load(f)
         f.close()
     return d
+
+
+def save_json(file: str, data):
+    with open(file, 'w', encoding='utf-8') as outfile:
+        json.dump(data, outfile, ensure_ascii=False, indent=2)
