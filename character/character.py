2e2923e923933751bf6328602bd622c99750f213
Vortex Acherontic <vortex@z-ray.de>
2019-06-30 13:08:34 +0200

    #16 improved character framework

--- character/character.py
+++ character/character.py
@@ -1,6 +1,7 @@
 import uuid
 
-from utils import hurby_utils, logger
+from utils import hurby_utils, logger, json_loader
+from utils.const import CONST
 
 
 class Character:
@@ -13,19 +14,19 @@ class Character:
     PERM_ADM = "administrator"
 
     def __init__(self):
-        self.credits = None
-        self.endurance = None
-        self.endurance_max = None
-        self.inventory = [None]
-        self.mails = None
-        self.twitchid = None
-        self.twitterid = None
-        self.discordid = None
-        self.youtubeid = None
-        self.name = None
-        self.can_do_mini_game = True
-        self.uuid = None
-        self.perm = Character.PERM_EVE
+        self.credits: int = None
+        self.endurance: int = None
+        self.endurance_max: int = None
+        self.inventory: list = None
+        self.mails: list[str] = None
+        self.twitchid: str = None
+        self.twitterid: str = None
+        self.discordid: str = None
+        self.youtubeid: str = None
+        self.name: str = None
+        self.can_do_mini_game: bool = True
+        self.uuid: str = None
+        self.perm: str = Character.PERM_EVE
 
     def init_default_character(self, name):
         logger.log(logger.INFO, "New default character: " + name)
@@ -57,7 +58,7 @@ class Character:
             if not self.mail_exists(mail):
                 hurby_utils.append_element_to_array(self.mails, mail)
 
-    def mail_exists(self, mail):
+    def mail_exists(self, mail) -> bool:
         if len(self.mails) > 0:
             for i in range(0, len(self.mails)):
                 if self.mails[0] == mail:
@@ -66,9 +67,20 @@ class Character:
         return False
 
     def parse_json(self, json):
-        pass
+        self.uuid = json["uuid"]
+        self.name = json["name"]
+        self.credits = json["credits"]
+        self.endurance = json["endurance_cur"]
+        self.endurance_max = json["endurance_max"]
+        self.discordid = json["discordid"]
+        self.twitchid = json["twitchid"]
+        self.youtubeid = json["youtubeid"]
+        self.twitterid = json["twitterid"]
+        self.mails = json["mail"]
+        self.inventory = json["inventory"]
+        self.perm = json["permission_level"]
 
-    def convert_to_json(self):
+    def convert_to_json(self) -> dict:
         text = {
             "uuid": self.uuid,
             "name": self.name,
@@ -84,3 +96,13 @@ class Character:
             "permission_level": self.perm
         }
         return text
+
+    def save(self):
+        data = self.convert_to_json()
+        file = CONST.DIR_CHARACTERS_ABSOLUTE + "/" + str(self.uuid) + ".json"
+        json_loader.save_json(file, data)
+
+    def load(self, json_file_name):
+        absolute_file = CONST.DIR_CHARACTERS_ABSOLUTE + "/" + json_file_name
+        json_data = json_loader.loadJSON(absolute_file)
+        self.parse_json(json_data)
--- character/character_manager.py
+++ character/character_manager.py
@@ -1,63 +1,41 @@
-import json
-
 from character.blacklist import Blacklist
 from character.character import Character
 from character.character_reference_table import CharacterReferenceTable
-from utils import logger, json_loader
-from utils.const import CONST
 
 
 class CharacterManager:
     ID_TYPE_TWITCH = "twitch"
     ID_TYPE_YOUTUBE = "yt"
+    ID_TYPE_TWITTER = "twitter"
+    ID_TYPE_PATREON = "patreon"
 
     def __init__(self):
-        self.chars = [None]
-        self.black_list = Blacklist()
-        self.ref_table = CharacterReferenceTable()
+        self.chars: list[Character] = None
+        self.black_list: Blacklist = Blacklist()
+        self.ref_table: CharacterReferenceTable = CharacterReferenceTable()
 
-    # This will check if a user with an id, wether it is twitch_hurby, YouTube or what ever, already is loaded
-    # Retruns true a matching character is oaded and false if not
     def get_char(self, user_id, id_type) -> Character:
         if id_type == CharacterManager.ID_TYPE_TWITCH:
-            return self.search_char_by_twitch_id(user_id)
+            return self._search_char_by_twitch_id(user_id)
 
-    # THis will load a matching JSON by it's id. Character jsons are just counted starting by 0
-    # 0.json would be the first ever created character
-    # 1.json would be the 2nd char and so on
-    # And will load it to the self.chars array
-    def load_char(self, json_id):
-        pass
-
-    # This will store a character on disk if a user will leave the stream or disconnect from discord
-    def save_char(self, char: Character):
-        data = char.convert_to_json()
-        file = CONST.DIR_CHARACTERS_ABSOLUTE + "/" + str(char.uuid) + ".json"
-        json_loader.save_json(file, data)
-        # with open(file, 'w', encoding='utf-8') as outfile:
-        # json.dump(data, outfile, ensure_ascii=False, indent=2)
-
-    # This will create a new default character based on the users id and the id type.
-    # The ID type is basically determined by the platfrom the user has appeard
-    def new_char(self, user_id, user_id_type) -> Character:
-        char = Character()
-        char.init_default_character(user_id)
-        if user_id_type == CharacterManager.ID_TYPE_TWITCH:
-            char.twitchid = user_id
-        self.save_char(char)
-        return char
-
-
-    def get_black_list(self):
+    def get_black_list(self) -> Blacklist:
         return self.black_list
 
-    def search_char_by_twitch_id(self, id):
+    def _search_char_by_twitch_id(self, user_id) -> Character:
         if self.chars is not None:
             for i in range(0, len(self.chars)):
-                cur = self.chars[i]
-                if cur is not None and cur.twitchid == id:
-                    return cur
-        tmp = self.new_char(user_id=id, user_id_type=CharacterManager.ID_TYPE_TWITCH)
-        self.chars.append(tmp)
-        self.ref_table.add_to_ref_table(str(tmp.uuid), tmp.twitchid)
+                if self.chars[i].twitchid == user_id:
+                    return self.chars[i]
+        tmp = Character()
+        json_file_name = self._check_reference_table(user_id)
+        if json_file_name is not None:
+            tmp.load(json_file_name)
+        else:
+            tmp.init_default_character(user_id)
+            tmp.set_twitch_id(user_id)
+            self.ref_table.add_to_ref_table(user_id, tmp.uuid)
+            tmp.save()
         return tmp
+
+    def _check_reference_table(self, user_id) -> str:
+        return self.ref_table.get_json_file_by_user_id(user_id)
