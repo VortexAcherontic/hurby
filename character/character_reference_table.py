842e5aea566d72bf49cc4acc574e7330ff83c221
Vortex Acherontic <vortex@z-ray.de>
2019-06-30 13:08:05 +0200

    #21 Added character reference table

--- character/character_reference_table.py
+++ character/character_reference_table.py
@@ -8,32 +8,28 @@ class CharacterReferenceTable:
     REF_TABLE_FILE = CONST.DIR_CHARACTERS_ABSOLUTE + "/" + CONST.FILE_CHAR_REF_TABLE
 
     def __init__(self):
-        self.load_ref_table()
         self.map = None
+        self.load()
 
-    def load_ref_table(self):
+    def get_json_file_by_user_id(self, user_id):
+        if self.map is not None:
+            for x in self.map:
+                if x == user_id:
+                    logger.log(logger.INFO, "User known: " + user_id)
+                    return self.map[x]
+        else:
+            return None
+
+    def add_to_ref_table(self, user_id: str, uudi: str):
+        if self.map is None:
+            self.map = {}
+        self.map[user_id] = uudi + ".json"
+        self.save()
+
+    def load(self):
         self.map = json_loader.loadJSON(CharacterReferenceTable.REF_TABLE_FILE)
 
-    def get_json_by_user_id(self, user_id):
-        for x in self.map:
-            if x == user_id:
-                return map[x]
-        return None
-
-    def add_to_ref_table(self, uudi: str, user_id: str):
-        if self.map is not None:
-            if self.map[user_id] is None:
-                self.map.update({user_id: uudi})
-                self.save_ref_table()
-            else:
-                return
-        else:
-            if self.map is None:
-                self.map = {}
-            self.map[user_id] = uudi + ".json"
-            self.save_ref_table()
-
-    def save_ref_table(self):
+    def save(self):
         logger.log(logger.INFO, self.map)
         with open(CharacterReferenceTable.REF_TABLE_FILE, 'w') as fp:
             json.dump(self.map, fp)
