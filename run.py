1d57ba8f851cf490d1b52d46ea51dbe338fdc030
Vortex Acherontic <vortex@z-ray.de>
2019-07-02 02:08:36 +0200

    Imprved run script to use updated twitch config with streamer config

--- run.py
+++ run.py
@@ -4,4 +4,5 @@ from utils import logger
 logger.log(logger.INFO, "Starting Bot...")
 
 hurby = Hurby()
-irc_listener = hurby.get_twitch_receiver().get_twitch_irc_connector().start("#couchrebellen")
+irc_channel = hurby.twitch_receiver.twitch_conf.channel_name
+irc_listener = hurby.get_twitch_receiver().get_twitch_irc_connector().start(irc_channel)
