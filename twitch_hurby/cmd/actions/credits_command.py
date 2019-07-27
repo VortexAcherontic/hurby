c1371098c6ae63f4bd64380c27d99db262c07153
Vortex Acherontic <vortex@z-ray.de>
2019-07-27 17:57:26 +0200

    #27 Added credits command

--- twitch_hurby/cmd/actions/credits_command.py
+++ twitch_hurby/cmd/actions/credits_command.py
@@ -1,3 +1,5 @@
+import random
+
 from character.character import Character
 from twitch_hurby.cmd.abstract_command import AbstractCommand
 from twitch_hurby.cmd.enums.cmd_response_realms import CMDResponseRealms
@@ -14,6 +16,11 @@ class CreditsCommand(AbstractCommand):
         replies = json_data["reply"]
         AbstractCommand.__init__(self, trigger, cmd_type, cmd_realm, replies, cmd_perm)
         self.hurby = hurby
+        self.answers = json_data["answers"]
 
-    def do_command(self, params: list, character : Character):
-        pass
+    def do_command(self, params: list, character: Character):
+        irc = self.hurby.twitch_receiver.twitch_listener
+        cur_answer = self.answers[random.randint(0, len(self.answers) - 1)]
+        cur_answer = cur_answer.replace("$user_id", character.twitchid)
+        cur_answer = cur_answer.replace("$user_credits", str(character.credits))
+        irc.send_message(cur_answer)
