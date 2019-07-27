397eeb92b52c72e5f24b5b51809b48f251e74037
Vortex Acherontic <vortex@z-ray.de>
2019-07-27 14:11:16 +0200

    #14 improved cmd response realm handling

--- twitch_hurby/cmd/enums/cmd_response_realms.py
+++ twitch_hurby/cmd/enums/cmd_response_realms.py
@@ -4,9 +4,3 @@ from enum import Enum
 class CMDResponseRealms(Enum):
     GLOBAL = "global"
     WHISPER = "whisper"
-
-
-def get_realm(string: str):
-    for s in CMDResponseRealms:
-        if s == string:
-            return s
--- twitch_hurby/cmd/simple_response.py
+++ twitch_hurby/cmd/simple_response.py
@@ -1,7 +1,7 @@
 import random
 
 from twitch_hurby.cmd.abstract_command import AbstractCommand
-from twitch_hurby.cmd.enums import cmd_response_realms
+from twitch_hurby.cmd.enums.cmd_response_realms import CMDResponseRealms
 from twitch_hurby.cmd.enums.cmd_types import CMDType
 from twitch_hurby.cmd.enums.permission_levels import PermissionLevels
 
@@ -10,14 +10,11 @@ class SimpleResponse(AbstractCommand):
     def __init__(self, json_data, hurby):
         trigger = json_data["cmd"]
         cmd_type = CMDType(json_data["type"])
-        cmd_realm = cmd_response_realms.get_realm(json_data["realm"])
+        cmd_realm = CMDResponseRealms(json_data["realm"])
         cmd_perm = PermissionLevels(json_data["perm"])
         replies = json_data["reply"]
         AbstractCommand.__init__(self, trigger, cmd_type, cmd_realm, replies, cmd_perm)
         self.hurby = hurby
-
-    def get_cmd(self):
-        return self.cmd
 
     def do_command(self, params: list):
         irc = self.hurby.twitch_receiver.twitch_listener
