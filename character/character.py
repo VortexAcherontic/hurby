2f5e9006748ab28949e318c3f79764e7a0af4089
Vortex Acherontic <vortex@z-ray.de>
2019-07-27 17:09:07 +0200

    #16 Improved character framework

--- character/character.py
+++ character/character.py
@@ -1,5 +1,6 @@
 import uuid
 
+from character.user_id_types import UserIDType
 from twitch_hurby.cmd.enums.permission_levels import PermissionLevels
 from utils import hurby_utils, logger, json_loader
 from utils.const import CONST
@@ -17,15 +18,13 @@ class Character:
         self.twitterid: str = None
         self.discordid: str = None
         self.youtubeid: str = None
-        self.name: str = None
         self.can_do_mini_game: bool = True
         self.uuid: str = None
         self.perm: str = PermissionLevels.EVERYBODY
         self.last_seen = None
 
-    def init_default_character(self, name: str, permission_level: PermissionLevels):
-        logger.log(logger.INFO, "New character: " + name)
-        self.name = name
+    def init_default_character(self, user_id: str, permission_level: PermissionLevels, user_id_type: UserIDType):
+        logger.log(logger.INFO, "New character: " + user_id)
         self.credits = 100
         self.endurance = 100
         self.endurance_max = 100
@@ -35,7 +34,6 @@ class Character:
         self.twitterid = None
         self.discordid = None
         self.youtubeid = None
-        self.name = None
         self.can_do_mini_game = True
         self.uuid = str(uuid.uuid4())
         self.perm = permission_level.value
@@ -67,7 +65,6 @@ class Character:
 
     def parse_json(self, json):
         self.uuid = json["uuid"]
-        self.name = json["name"]
         self.credits = json["credits"]
         self.endurance = json["endurance_cur"]
         self.endurance_max = json["endurance_max"]
@@ -82,7 +79,6 @@ class Character:
     def convert_to_json(self) -> dict:
         text = {
             "uuid": self.uuid,
-            "name": self.name,
             "credits": self.credits,
             "endurance_cur": self.endurance,
             "endurance_max": self.endurance_max,
@@ -105,3 +101,4 @@ class Character:
         absolute_file = CONST.DIR_CHARACTERS_ABSOLUTE + "/" + json_file_name
         json_data = json_loader.loadJSON(absolute_file)
         self.parse_json(json_data)
+        logger.log(logger.INFO, "Loaded character: twitchID: " + self.twitchid)
--- character/character_manager.py
+++ character/character_manager.py
@@ -19,18 +19,12 @@ class CharacterManager:
         if id_type == UserIDType.TWITCH:
             return self._search_char_by_twitch_id(user_id)
 
+    def load_character(self, user_id: str, id_type: UserIDType):
+        if not self._is_character_loaded(user_id=user_id, id_type=id_type):
+            self._load_character(user_id=user_id, id_type=id_type)
+
     def get_black_list(self) -> Blacklist:
         return self.black_list
-
-    def _search_char_by_twitch_id(self, user_id) -> Character:
-        if self.chars is not None:
-            for i in range(0, len(self.chars)):
-                if self.chars[i].twitchid == user_id:
-                    return self.chars[i]
-        return None
-
-    def _check_reference_table(self, user_id) -> str:
-        return self.ref_table.get_json_file_by_user_id(user_id)
 
     def check_viewer_id(self, user_id_type: UserIDType, user_id):
         if user_id_type == UserIDType.TWITCH:
@@ -42,10 +36,14 @@ class CharacterManager:
 
     def create_new_character(self, user_id_type: UserIDType, user_id: str, permission_level: PermissionLevel):
         tmp = Character()
-        tmp.init_default_character(user_id, permission_level)
+        tmp.init_default_character(user_id,  permission_level=permission_level, user_id_type=user_id_type)
         if user_id_type == UserIDType.TWITCH:
             tmp.set_twitch_id(user_id)
         self.ref_table.add_to_ref_table(user_id, tmp.uuid)
+        if self.chars is None:
+            self.chars = [tmp]
+        else:
+            self.chars.append(tmp)
         tmp.save()
 
     def unload_offline_character(self, user_ids: List[str], user_id_type: UserIDType):
@@ -55,6 +53,7 @@ class CharacterManager:
             for c in self.chars:
                 char_in_uids = False
                 for uid in user_ids:
+                    logger.log(logger.INFO, "Checking: " + uid)
                     if user_id_type == UserIDType.TWITCH:
                         if c.twitchid == uid:
                             char_in_uids = True
@@ -62,3 +61,32 @@ class CharacterManager:
                     logger.log(logger.INFO, "Character: " + c.twitchid + " is offline, unloading ....")
                     c.save()
                     self.chars.remove(c)
+
+    def _search_char_by_twitch_id(self, user_id) -> Character:
+        if self.chars is not None:
+            for i in range(0, len(self.chars)):
+                if self.chars[i].twitchid == user_id:
+                    return self.chars[i]
+        return None
+
+    def _check_reference_table(self, user_id) -> str:
+        return self.ref_table.get_json_file_by_user_id(user_id)
+
+    def _load_character(self, user_id: str, id_type: UserIDType):
+        if not self._is_character_loaded(user_id, id_type):
+            user_json_file = self.ref_table.get_json_file_by_user_id(user_id)
+            tmp = Character()
+            tmp.load(user_json_file)
+            if self.chars is None:
+                self.chars = [tmp]
+            else:
+                self.chars.append(tmp)
+
+    def _is_character_loaded(self, user_id: str, id_type: UserIDType):
+        if self.chars is not None:
+            for i in range(0, len(self.chars)):
+                if id_type == UserIDType.TWITCH:
+                    if self.chars[i].twitchid == user_id:
+                        logger.log(logger.INFO, "Character: " + user_id + " is already loaded")
+                        return True
+        return False
