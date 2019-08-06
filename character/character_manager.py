b14d15865f131d0d5bfc47e66cd0af84469646fe
Vortex Acherontic <vortex@z-ray.de>
2019-08-06 20:27:25 +0200

    #16 Fixed loaded chars not added to loaded chars table

--- character/character_manager.py
+++ character/character_manager.py
@@ -23,14 +23,14 @@ class CharacterManager:
             if self._is_in_reference_table(user_id):
                 json_file = self.ref_table.get_json_file_by_user_id(user_id)
                 tmp_char.load(json_file)
-                self.chars.append(tmp_char)
+                self._add_char_to_table(tmp_char)
                 return tmp_char
             else:
                 tmp_char.init_default_character(user_id, permission_level, user_id_type)
-                self.chars.append(tmp_char)
+                self._add_char_to_table(tmp_char)
                 return tmp_char
         else:
-            self.chars.append(tmp_char)
+            self._add_char_to_table(tmp_char)
             return tmp_char
 
     def unload_offline_characters(self, user_ids: list, id_type: UserIDType):
@@ -41,6 +41,13 @@ class CharacterManager:
                     logger.log(logger.INFO, "User offline, unloading: " + cur_char.twitchid)
                     cur_char.save()
                     self.chars.remove(cur_char)
+
+    def _add_char_to_table(self, char: Character):
+        if self.chars is None:
+            self.chars = [char]
+        elif self.chars is list:
+            self.chars.append(char)
+
 
     def _is_chars_in_user_ids(self, user_ids: [str], char: Character, user_id_type: UserIDType):
         for x in user_ids:
@@ -64,21 +71,22 @@ class CharacterManager:
         return False
 
     def _search_loaded_characters(self, user_id: str, user_id_type: UserIDType):
-        if self.chars is not None:
+        if self.chars is not None :
             for tmp in self.chars:
-                if user_id_type == UserIDType.TWITCH:
-                    if user_id == tmp.twitchid:
-                        return tmp
-                elif user_id_type == UserIDType.TWITTER:
-                    pass
-                elif user_id_type == UserIDType.PATREON:
-                    pass
-                elif user_id_type == UserIDType.YOUTUBE:
-                    pass
-                elif user_id_type == UserIDType.STEAM:
-                    pass
-                elif user_id_type == UserIDType.DISCORD:
-                    pass
+                if tmp is not None:
+                    if user_id_type == UserIDType.TWITCH:
+                        if user_id == tmp.twitchid:
+                            return tmp
+                    elif user_id_type == UserIDType.TWITTER:
+                        pass
+                    elif user_id_type == UserIDType.PATREON:
+                        pass
+                    elif user_id_type == UserIDType.YOUTUBE:
+                        pass
+                    elif user_id_type == UserIDType.STEAM:
+                        pass
+                    elif user_id_type == UserIDType.DISCORD:
+                        pass
         return None
 
     def _is_in_reference_table(self, user_id: str):
