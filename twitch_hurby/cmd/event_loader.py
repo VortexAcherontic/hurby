928ab56821118137a8d07f798f4b098eb0cb5a05
Vortex Acherontic <vortex@z-ray.de>
2019-10-13 15:13:20 +0200

    #20 Inital Event setup (WIP)

---
+++ twitch_hurby/cmd/event_loader.py
@@ -0,0 +1,5 @@
+from config.bot_config import BotConfig
+
+
+def create_event(json_data, bot_config: BotConfig, hurby):
+    pass
--- twitch_hurby/twitch_config.py
+++ twitch_hurby/twitch_config.py
@@ -1,13 +1,15 @@
 from os import listdir
 from os.path import isfile, join
 
-from twitch_hurby.cmd import cmd_loader
+from config.bot_config import BotConfig
+from twitch_hurby.cmd import cmd_loader, event_loader
 from utils import logger, json_loader
 from utils.const import CONST
 
 
 class TwitchConfig:
     CMD_PATH = CONST.DIR_APP_DATA_ABSOLUTE + "/templates/commands/twitch/"
+    EVENT_PATH = CONST.DIR_APP_DATA_ABSOLUTE + "/templates/events/twitch/"
     HOST = "irc.twitch.tv"
     PORT = 6667
 
@@ -17,6 +19,8 @@ class TwitchConfig:
         twitch_json = json_loader.load_json(config_file)
         self.onlyfiles = [f for f in listdir(TwitchConfig.CMD_PATH) if isfile(join(TwitchConfig.CMD_PATH, f))]
         self.cmds = [None] * len(self.onlyfiles)
+        self.onlyfiles_events = [f for f in listdir(TwitchConfig.EVENT_PATH) if isfile(join(TwitchConfig.EVENT_PATH, f))]
+        self.events = [None] * len(self.onlyfiles_events)
         self.oauth_token = twitch_json["oauth_token"]
         self.channel_name = twitch_json["channel_name"]
         self.streamer = twitch_json["streamer"]
@@ -32,6 +36,7 @@ class TwitchConfig:
         logger.log(logger.INFO, self.cron_jobs)
         self.bot_username = self.hurby.get_bot_config().botname
         self.load_cmds()
+        self.load_events()
 
     def load_cmds(self):
         self.cmds = [None] * len(self.onlyfiles)
@@ -40,6 +45,14 @@ class TwitchConfig:
                 cmd_json = json_loader.load_json(TwitchConfig.CMD_PATH + self.onlyfiles[i])
                 self.cmds[i] = cmd_loader.create_cmd(cmd_json, self.hurby.get_bot_config(), self.hurby)
         logger.log(logger.INFO, str(len(self.cmds)) + " commands loaded")
+
+    def load_events(self):
+        if self.hurby.get_bot_config().modules[BotConfig.MODULE_EVENTS]:
+            self.events = [None] * len(self.onlyfiles_events)
+            for i in range(0, len(self.onlyfiles_events)):
+                if self.onlyfiles_events[i].endswith(".json"):
+                    event_json = json_loader.load_json(TwitchConfig.EVENT_PATH + self.onlyfiles_events[i])
+                    self.events[i] = event_loader.create_event(event_json, self.hurby.get_bot_config(), self.hurby)
 
     def get_cmds(self) -> list:
         return self.cmds
