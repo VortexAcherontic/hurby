0d9cc219e6bbc48a95c9830b183d4fdd7ead11b1
Vortex Acherontic <vortex@z-ray.de>
2019-10-13 15:14:27 +0200

    #20 Inital Event setup (WIP)

--- config/bot_config.py
+++ config/bot_config.py
@@ -12,12 +12,13 @@ class BotConfig:
     MODULE_STEAM = 4
     MODULE_TRELLO = 5
     MODULE_MINIGAME = 6
+    MODULE_EVENTS = 7
 
     def __init__(self):
         config_file = CONST.DIR_CONF_ABSOLUTE + "/" + CONST.FILE_CONF_HURBY
         bot_json = json_loader.load_json(config_file)
         self.botname = bot_json["botname"]
-        self.modules = [None] * 7
+        self.modules = [None] * 8
         self.modules[BotConfig.MODULE_TWITCH] = bot_json["modules"]["twitch"]
         self.modules[BotConfig.MODULE_TWITTER] = bot_json["modules"]["twitter"]
         self.modules[BotConfig.MODULE_YOUTUBE] = bot_json["modules"]["youtube"]
@@ -25,6 +26,7 @@ class BotConfig:
         self.modules[BotConfig.MODULE_STEAM] = bot_json["modules"]["steam"]
         self.modules[BotConfig.MODULE_TRELLO] = bot_json["modules"]["trello"]
         self.modules[BotConfig.MODULE_MINIGAME] = bot_json["modules"]["minigame"]
+        self.modules[BotConfig.MODULE_EVENTS] = bot_json["modules"]["events"]
         self.bot_name_in_reply = bot_json["bot_name_in_reply"]
         self.unknown_cmd_response = bot_json["unknown_command_response"]
 
