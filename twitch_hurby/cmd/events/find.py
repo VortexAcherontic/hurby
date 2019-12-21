6a989369b4c747fe1769de7ed978e8c3a695facc
Vortex Acherontic <vortex@z-ray.de>
2019-12-21 23:35:53 +0100

    Fixed find event not able to deal with None Characters in case of blacklist

--- twitch_hurby/cmd/events/find.py
+++ twitch_hurby/cmd/events/find.py
@@ -26,28 +26,29 @@ class EventFind:
 
     def issue_event(self):
         all_chars = self.hurby.char_manager.chars
-        lucky_one: Character = all_chars[random.randint(0, len(all_chars) - 1)]
-        min_value = self.credit_find_min
-        max_value = self.credit_find_max
-        found_value = random.randint(min_value, max_value)
-
-        while lucky_one is None:
+        if all_chars is not None:
             lucky_one: Character = all_chars[random.randint(0, len(all_chars) - 1)]
-        if (random.random() > 0.5) & self.find_items:
-            items = [BaseItem]
-            items_value = _value_of_items(items)
-            while items_value > found_value:
-                rnd_item: BaseItem = self.hurby.item_manager.get_random_item
-                tmp_sum = items_value + rnd_item.value
-                if tmp_sum <= found_value:
-                    items.append(rnd_item)
-        else:
-            lucky_one.credits += found_value
-            lucky_one.save()
-            msg = hurby_utils.get_random_reply(self.event_end)
-            msg = msg.replace("$user", lucky_one.twitchid)
-            cred_repl = hurby_utils.get_random_reply(self.loot_credits)
-            cred_repl = cred_repl.replace("$amount", str(found_value))
-            msg = msg.replace("$loot", cred_repl)
-            irc = self.hurby.twitch_receiver.twitch_listener
-            irc.send_message(msg)
+            min_value = self.credit_find_min
+            max_value = self.credit_find_max
+            found_value = random.randint(min_value, max_value)
+
+            while lucky_one is None:
+                lucky_one: Character = all_chars[random.randint(0, len(all_chars) - 1)]
+            if (random.random() > 0.5) & self.find_items:
+                items = [BaseItem]
+                items_value = _value_of_items(items)
+                while items_value > found_value:
+                    rnd_item: BaseItem = self.hurby.item_manager.get_random_item
+                    tmp_sum = items_value + rnd_item.value
+                    if tmp_sum <= found_value:
+                        items.append(rnd_item)
+            else:
+                lucky_one.credits += found_value
+                lucky_one.save()
+                msg = hurby_utils.get_random_reply(self.event_end)
+                msg = msg.replace("$user", lucky_one.twitchid)
+                cred_repl = hurby_utils.get_random_reply(self.loot_credits)
+                cred_repl = cred_repl.replace("$amount", str(found_value))
+                msg = msg.replace("$loot", cred_repl)
+                irc = self.hurby.twitch_receiver.twitch_listener
+                irc.send_message(msg)
