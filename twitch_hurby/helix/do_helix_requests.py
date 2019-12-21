56da2306666e7e10a542446dde6dcc646654af64
Vortex Acherontic <vortex@z-ray.de>
2019-12-21 23:43:42 +0100

    #63 Code reformatting

--- twitch_hurby/helix/do_helix_requests.py
+++ twitch_hurby/helix/do_helix_requests.py
@@ -9,14 +9,14 @@ def get(url, twitch_config: TwitchConfig):
     token = twitch_config.access_token
     response = _do_request(url, token)
     if response.status_code == 401:
-        logger.log(logger.WARN,
-                   "Twitch Helix request failed for url: " + url + " Message: " + response.text + " token: " + token)
+        logger.log(logger.WARN, "Twitch Helix request failed for url: " + url
+                   + " Message: " + response.text + " token: " + token)
         get_bearer_access_token(twitch_config)
         token = twitch_config.access_token
         response = _do_request(url, token)
         if not (200 <= response.status_code <= 226):
-            logger.log(logger.ERR,
-                       "Twitch Helix request failed for url: " + url + " Message: " + response.text + " token: " + token)
+            logger.log(logger.ERR, "Twitch Helix request failed for url: " + url
+                       + " Message: " + response.text + " token: " + token)
             exit(0)
     return response
 
