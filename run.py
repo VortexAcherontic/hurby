6049c8743fb1d0bc81b6a2d9898d5367262465f8
Vortex Acherontic <vortex@z-ray.de>
2019-12-22 14:25:46 +0100

    #69 WIP multiple irc channel

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ from utils import logger
 logger.log(logger.INFO, "Starting Bot...")
 
 hurby = Hurby()
-irc_channel = hurby.twitch_receiver.twitch_conf.channel_name
-hurby.get_twitch_receiver().get_twitch_irc_connector().start(irc_channel)
+irc_channels = hurby.twitch_receiver.twitch_conf.channel_names
+hurby.get_twitch_receiver().get_twitch_irc_connector().start(irc_channels)
 hurby.char_manager.black_list.init()
