381e77106aef9f81e8786e06a566512388e89726
Vortex Acherontic <vortex@z-ray.de>
2020-06-11 13:47:19 +0200

    #90 Updated for new Helix API

--- twitch_hurby/helix/do_helix_requests.py
+++ twitch_hurby/helix/do_helix_requests.py
@@ -7,13 +7,14 @@ from utils import logger
 
 def get(url, twitch_config: TwitchConfig):
     token = twitch_config.access_token
-    response = _do_request(url, token)
+    clientid = twitch_config.client_id
+    response = _do_request(url, token, clientid)
     if response.status_code == 401:
         logger.log(logger.WARN, "Twitch Helix request failed for url: " + url
                    + " Message: " + response.text)
         get_bearer_access_token(twitch_config)
         token = twitch_config.access_token
-        response = _do_request(url, token)
+        response = _do_request(url, token, clientid)
         if not (200 <= response.status_code <= 226):
             logger.log(logger.ERR, "Twitch Helix request failed for url: " + url
                        + " Message: " + response.text)
@@ -21,5 +22,5 @@ def get(url, twitch_config: TwitchConfig):
     return response
 
 
-def _do_request(url: str, token: str):
-    return requests.get(url, headers={"Authorization": "Bearer " + token})
+def _do_request(url: str, token: str, clientid: str):
+    return requests.get(url, headers={"Authorization": "Bearer " + token, "Client-ID": clientid})
