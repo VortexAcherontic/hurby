4e7ff9df928e2715d629ecbd888243540ba35696
Vortex Acherontic <vortex@z-ray.de>
2020-11-14 10:11:10 +0100

    #130 Added backup support for JSON file loading and writing

--- utils/json_loader.py
+++ utils/json_loader.py
@@ -1,4 +1,6 @@
 import json
+import os
+import shutil
 
 from utils import logger
 
@@ -11,9 +13,20 @@ def load_json(file: str) -> dict:
             f.close()
         return d
     except json.decoder.JSONDecodeError as e:
-        logger.log(logger.ERR, "Could not decode json file: "+file)
+        if _restore_backup(file):
+            return load_json(file)
+        logger.log(logger.ERR, "Could not decode json file: " + file)
 
 
 def save_json(file: str, data):
+    shutil.copyfile(file, file + ".bak")
     with open(file, 'w+', encoding='utf-8') as outfile:
         json.dump(data, outfile, ensure_ascii=False, indent=2)
+    os.remove(file+".bak")
+
+
+def _restore_backup(file: str):
+    if os.path.isfile(file+".bak"):
+        shutil.move(file+".bak", file)
+        return True
+    return False
