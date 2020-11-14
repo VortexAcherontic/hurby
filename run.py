ed368c9f277548410886bab1049a0262517ec63a
Vortex Acherontic <vortex@z-ray.de>
2020-11-14 09:53:19 +0100

    Removed webapp due to dedicated milestone-beta branch

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from config.bot_config import BotConfig
 from hurby import Hurby
 from utils import logger
-from webapp.app import run_flask
 
 try:
     logger.log(logger.INFO, "Starting Bot...")
@@ -10,8 +9,5 @@ try:
     irc_channels = hurby.twitch_receiver.twitch_conf.channel_names
     hurby.get_twitch_receiver().get_twitch_irc_connector().start(irc_channels)
     hurby.char_manager.black_list.init()
-    if hurby.botConfig.modules[BotConfig.MODULE_WEBSERVER]:
-        logger.log(logger.INFO, "Starting flask server...")
-        run_flask(hurby)
 except Exception as e:
     logger.log(logger.FATAL, str(e))
